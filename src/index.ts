062a5539b1e210e7c0b34815567d3438fe991489
ArakiTakaki <araki_takaki@yahoo.co.jp>
2021-05-05 05:47:03 +0900

    update lit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
-import litMarkdown from './components/litMarkdown';
 import styles from './index.proxy.css';
+import './sections/markdown';
 
 @customElement("my-element-2")
 export class Hoge extends LitElement {
@@ -10,30 +10,26 @@ export class Hoge extends LitElement {
   }
 }
 
+const content = `
+# markdown
+
+markdown content
+`
+
 @customElement("my-element")
 export class MyElement extends LitElement {
   static styles = styles;
 
   @property() greeting = "Hello";
   @property() planet = "World";
+
   render() {
-    const bar = litMarkdown(`
-## buzz
-
-bar
-bizz
-
-markdown text
-
-<my-element-2></my-element-2>
-
-`)
     return html`
 <span @click=${this.togglePlanet}>
   ${this.greeting}
-  ${bar}
   <h2>buzzz bar</h2>a
   <my-element-2></my-element-2>
+  <markdown-content content=${content}></markdown-content>
   <span class="planet">${this.planet}</span>
 </span>
 `;
--- src/sections/markdown.ts
+++ src/sections/markdown.ts
@@ -1,63 +1,15 @@
-import {LitElement, html, css} from 'lit';
+import {LitElement, html } from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import litMarkdown from '../components/litMarkdown';
-// import marked from 'marked';
 
-@customElement("markdown")
+@customElement("markdown-content")
 export class MyElement extends LitElement {
-  // Styles are scoped to this element: they won't conflict with styles
-  // on the main page or in other components. Styling API can be exposed
-  // via CSS custom properties.
-  static styles = css`
-    :host {
-      display: inline-block;
-      padding: 10px;
-      background: lightgray;
-    }
-
-    .planet {
-      color: var(--planet-color, blue);
-      cursor: pointer;
-    }
-  `;
-
-
-  // Define reactive properties--updating a reactive property causes
-  // the component to update.
-  @property() greeting = "Hello";
-  @property() planet = "World";
-
-  // The render() method is called any time reactive properties change.
-  // Return HTML in a string template literal tagged with the `html`
-  // tag function to describe the component's internal DOM.
-  // Expressions can set attribute values, proeprty values, event handlers,
-  // and child nodes/text.
+  @property() content = "";
   render() {
-    const bar = litMarkdown(`
-## buzz
-
-bar
-bizz
-
-markdown text
-
-<my-element-2></my-element-2>
-
-`)
     return html`
-<span @click=${this.togglePlanet}>
-  ${this.greeting}
-  ${bar}
-  <h2>buzzz</h2>a
-  <my-element-2></my-element-2>
-  <span class="planet">${this.planet}</span>
-</span>
-`;
-  }
-
-  // Event handlers can update the state of @properties on the element
-  // instance, causing it to re-render
-  togglePlanet() {
-    this.planet = this.planet === "World" ? "Mars" : "World";
+      <div>
+        ${litMarkdown(this.content)}
+      </div>
+    `;
   }
 }
